05f14045708d6bb25d3a14a5dbea7f341601e496
Maus <mvds@penthion.nl>
2015-03-23 09:35:19 +0000

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :description, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'Moet een URL zijn voor een GIF,JPG of een PNG bestand.'
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -24,12 +24,12 @@ class ProductTest < ActiveSupport::TestCase
                           image_url:   "zzz.jpg")
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
+    assert_equal ["Bedrag moet groter zijn dan 0.01"],
       product.errors[:price]
 
     product.price = 0
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], 
+    assert_equal ["Bedrag moet groter zijn dan 0.01"], 
       product.errors[:price]
 
     product.price = 1
