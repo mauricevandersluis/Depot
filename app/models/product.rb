091881a4ea5b8ddfe3d00e1023eb8eed0e5afde4
Maus <mvds@penthion.nl>
2015-03-23 08:58:00 +0000

    Changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :description, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'Moet een URL zijn voor een GIF,JPG of een PNG bestand.'
+  }
 end
